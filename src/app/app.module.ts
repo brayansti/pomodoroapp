b73cb981df7b8fdcaf3ed946e6b442e5bbb3d4db
Brayan Camargo <brayan440@hotmail.com>
2020-01-16 21:58:04 -0500

    Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { OptionsComponent } from './components/options/options.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OptionsComponent
   ],
   imports: [
     BrowserModule,
